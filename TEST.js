ea605621a8d48c6f58bcd543fb978717d0e27b47
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2022-11-13 20:55:14 +0700

    Testing

---
+++ TEST.js
@@ -0,0 +1,21 @@
+params = {
+    'preview': '1',
+    'order': 'name',
+    'page': '1',
+    'page_size': '10',
+    'levels': '2'
+}
+
+params = {
+    'page': '1',
+    'page_size': '10',
+    'fields.name': 'Amongus'
+}
+
+import requests
+
+payload = {
+    "name": "Amongmongmongus"
+}
+
+response = requests.get("https://api.buttercms.com/v2/content/apps/330939/", headers = { "Authorization": "b41a7496d852160b8370eab0bcabc25635737bc8" })
